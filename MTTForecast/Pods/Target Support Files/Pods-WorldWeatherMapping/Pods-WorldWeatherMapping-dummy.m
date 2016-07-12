5fe07dca634ef58fb70074fee27fddb84ca64aa8
Esteban Torres <me@estebantorr.es>
2016-07-12 16:48:24 -0600

    Fixes the ignores

--- MTTForecast/Pods/Target Support Files/Pods-WorldWeatherMapping/Pods-WorldWeatherMapping-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WorldWeatherMapping : NSObject
-@end
-@implementation PodsDummy_Pods_WorldWeatherMapping
-@end
--- MTTForecast/Pods/Target Support Files/Pods-WorldWeatherMappingTests/Pods-WorldWeatherMappingTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WorldWeatherMappingTests : NSObject
-@end
-@implementation PodsDummy_Pods_WorldWeatherMappingTests
-@end
